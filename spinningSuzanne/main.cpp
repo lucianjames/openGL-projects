ae4b302254d1097f94ca0e19aa6e3e914575a696
LJ3D <65134690+LJ3D@users.noreply.github.com>
2022-06-30 18:21:47 +0100

    Remove GLM. Use pacman instead
    I dont care if it makes compiling my code harder, i want my repo to look cleaner

--- spinningSuzanne/main.cpp
+++ spinningSuzanne/main.cpp
@@ -3,9 +3,9 @@
 #include <iostream>
 #include <math.h>
 
-#include "glm/glm.hpp"
-#include "glm/gtc/matrix_transform.hpp"
-#include "glm/gtc/type_ptr.hpp"
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 #include "initHelper.h"
 #include "shaderClass.h"
