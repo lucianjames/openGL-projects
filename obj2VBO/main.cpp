578cee9e421f7e091486325f44170780b748aba4
LJ3D <65134690+LJ3D@users.noreply.github.com>
2022-07-02 13:49:19 +0100

    Can make basic VBO

--- obj2VBO/main.cpp
+++ obj2VBO/main.cpp
@@ -73,7 +73,7 @@ private:
     template<typename T>
     void printVector(const std::vector<T>& data){
         for(int i = 0; i < data.size(); i++){
-            std::cout << data[i] << " ";
+            std::cout << data[i] << ", ";
         }
         std::cout << std::endl;
     }
@@ -84,12 +84,16 @@ public:
     std::vector<float> textureCoords;
     std::vector<vertexIndices> indices; // Index data
 
+    std::vector<float> VBO; // The actual VBO data derived from the above data.
+    std::vector<int> VBO_indices; // The indices of the VBO data derived from the above data.
+
     obj_file(const std::string fileName){
         readObjValues(fileName, "v ", 3, positions);
         readObjValues(fileName, "vn", 3, normals);
         readObjValues(fileName, "vt", 2, textureCoords);
         readObjIndices(fileName, indices);
     }
+
     void debugPrintData(){
         // Print position values
         std::cout << "Positions: " << std::endl;
@@ -100,6 +104,7 @@ public:
         // Print texture coordinate values
         std::cout << "Texture Coords: " << std::endl;
         printVector(textureCoords);
+        std::cout << "==========================================================" << std::endl;
         // Print vertex indices
         std::cout << "Vertex Indices: " << std::endl;
         for(int i = 0; i < indices.size(); i++){
@@ -109,7 +114,6 @@ public:
                 std::cout << std::endl;
             }
         }
-        std::cout << "End of vertex indices" << std::endl;
         std::cout << "Normal Indices: " << std::endl;
         for(int i = 0; i < indices.size(); i++){
             std::cout << indices[i].normalIndex << ", ";
@@ -118,13 +122,59 @@ public:
                 std::cout << std::endl;
             }
         }
-        std::cout << "End of normal indices" << std::endl;
+        std::cout << "Texture Indices: " << std::endl;
+        for(int i = 0; i < indices.size(); i++){
+            std::cout << indices[i].textureIndex << ", ";
+            // New line every 3 indices.
+            if(i % 3 == 2){
+                std::cout << std::endl;
+            }
+        }
     }
+
+    void assembleVBO(){
+        // 1: Create a VBO with all vertex data (expand the indices out to the full number of vertices)
+        // Set indices to count up from 0 to the number of vertices. (Will be optimised later on)
+
+        this->VBO.clear();
+        int vertSize = 3 + 3 + 2; // 3 floats for position, 3 floats for normal, 2 floats for texture coordinate. !!!!!! HARD CODE !!!!!!
+        this->VBO.resize(vertSize * indices.size()); // Resize the VBO to the correct size.
+        this->VBO_indices.clear();
+        this->VBO_indices.resize(positions.size() / 3); // The number of vertices is the number of positions divided by 3 because each vertex has 3 float values.
+
+        // !!!!!! HARD CODED - IDEALLY CAN ADAPT IF ANY ATTRIBUTE IS MISSING !!!!!!
+        for(int i=0; i < this->VBO.size() / vertSize; i++){
+            // Read position data from the indices.
+            this->VBO[(i * vertSize) + 0] = positions[((this->indices[i].vertexIndex-1) * 3) + 0]; // The -1 is because the indices start at 1, not 0. (Which is fucking retarded)
+            this->VBO[(i * vertSize) + 1] = positions[((this->indices[i].vertexIndex-1) * 3) + 1];
+            this->VBO[(i * vertSize) + 2] = positions[((this->indices[i].vertexIndex-1) * 3) + 2];
+            // Read normal data
+            this->VBO[(i * vertSize) + 3] = normals[((this->indices[i].normalIndex-1) * 3) + 0];
+            this->VBO[(i * vertSize) + 4] = normals[((this->indices[i].normalIndex-1) * 3) + 1];
+            this->VBO[(i * vertSize) + 5] = normals[((this->indices[i].normalIndex-1) * 3) + 2];
+            // Read texCoord data
+            this->VBO[(i * vertSize) + 6] = textureCoords[((this->indices[i].textureIndex-1) * 2) + 0];
+            this->VBO[(i * vertSize) + 7] = textureCoords[((this->indices[i].textureIndex-1) * 2) + 1];
+        }
+
+        // Print the entire VBO
+        std::cout << "VBO: " << std::endl;
+        for(int i = 0; i < this->VBO.size(); i++){
+            std::cout << this->VBO[i] << ", ";
+            // New line every vertSize indices.
+            if(i % vertSize == vertSize - 1){
+                std::cout << std::endl;
+            }
+        }
+        std::cout << std::endl;
+    }
+
 };
 
 
 int main(){
-    obj_file obj("../cube.obj");
-    obj.debugPrintData();
+    obj_file obj("../triangle.obj");
+    //obj.debugPrintData();
+    obj.assembleVBO();
     return 0;
 }
