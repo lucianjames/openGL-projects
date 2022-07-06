8577b2aea8bf4b03fb92face2a8e3fbd1ae74eed
LJ3D <65134690+LJ3D@users.noreply.github.com>
2022-07-06 22:15:46 +0100

    Remove outdated comments

--- obj2VBO/main.cpp
+++ obj2VBO/main.cpp
@@ -3,11 +3,6 @@
 #include "objVBO.h"
 
 // Read an OBJ file and output a file which can easily be read as a VBO (One line contains all vertex information, another the indices)
-// This program needs to:
-// 1: Read all data from an obj file
-// 2: Create a VBO with all vertex data (expand the indices out to the full number of vertices)
-// 3: Prune the VBO to remove duplicate vertices
-// 4: Create a VBO to make up for the pruning
 
 int main(int argc, char** argv){
     // Error checking
