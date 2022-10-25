05bf2a1e13615ae0e77c914c5a6c4a640dc6f3a4
LJ3D <65134690+LJ3D@users.noreply.github.com>
2022-10-25 23:26:24 +0100

    Getting closer to working, but not quite

--- spinningSuzanne/initHelper.h
+++ spinningSuzanne/initHelper.h
@@ -12,6 +12,8 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height){
 
 void initGLFW(){
     glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 }
 
